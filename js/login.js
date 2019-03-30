fde51de87caa24d854c1896924535dbbadd3cbd1
Lzy <292424886@qq.com>
2019-03-30 15:20:18 +1100

    modify login.js
    修改了跳转时间

--- js/login.js
+++ js/login.js
@@ -2,9 +2,6 @@ $('#login-button').click(function (event) {
         event.preventDefault();
         $('form').fadeOut(500);
         $('.wrapper').addClass('form-success');
-        setTimeout(function(){
-            location.href="BirthdayCake.html";
-        },3);
-
+        location.href="BirthdayCake.html";
 });
 
